3341dbc2cdd0c16e089caa4b8a0a2aa44f0ac4b0
haga-itsuki <haga-itsuki@dmm.com>
2020-08-29 02:01:09 +0900

    fix: rm storage

--- index.ts
+++ index.ts
@@ -1,87 +1,21 @@
 import simpleGit from "simple-git";
-import { isRight } from 'fp-ts/Either'
-import prompts from "prompts"
-import * as iots from 'io-ts'
-import fs from "fs"
-import path from "path"
+import prompts from "prompts";
+
 const git = simpleGit().outputHandler((_, stdout, stderr) => {
     stdout.pipe(process.stdout)
     stderr.pipe(process.stderr)
 })
 const gitWithoutStdout = simpleGit();
 
-interface GitHistoryRepository {
-    add(name: string): void;
-    getAll(): string[];
-}
-
-const IoData = iots.type({
-    branchHistory: iots.array(iots.string),
-})
-type Data = iots.TypeOf<typeof IoData>
-
-class FileGitHistoryRepository implements GitHistoryRepository {
-    private filepath: string = path.resolve("./storage/data.json")
-    constructor(private currentPath: string) {
-        console.log(this.filepath)
-        if (!fs.existsSync(this.filepath)) {
-            
-            fs.writeFileSync(this.filepath, JSON.stringify({}))
-        }
-    }
-    private readFile(): Data {
-        const json = JSON.parse(fs.readFileSync(this.filepath).toString())
-        const data = json[this.currentPath]
-        if(!data){
-            return {
-                branchHistory:[]
-            }
-        }
-        const decoded = IoData.decode(data)
-        if (isRight(decoded)) {
-            return decoded.right
-        } else {
-            throw new Error("Type Error")
-        }
-    }
-    private writeFile(data: Data): void {
-        if(IoData.decode(data)._tag === "Right"){
-            const prev = JSON.parse(fs.readFileSync(this.filepath).toString())
-            prev[this.currentPath] = data
-            fs.writeFileSync(this.filepath, JSON.stringify(prev))
-        }
-        throw new Error("Type Error")
-        
-    }
-    getAll(): string[] {
-        return this.readFile().branchHistory
-    }
-    add(name: string): void {
-        const data = this.readFile()
-        const res = {
-            ...data,
-            branchHistory: [name, ...data.branchHistory.filter(e => e !== name)]
-        }
-        this.writeFile(res)
-    }
-}
-const repo = new FileGitHistoryRepository(process.cwd())
-
 const checkout = async () => {
-    const {all,current} = await gitWithoutStdout.branch();
-    const saved = repo.getAll().reduce<Record<string,number | undefined>>((prev,cur,index) => {
-        return {
-            ...prev,
-            [cur]:index
-        }
-    },{})
-    const getPriority = (name: string):number => {
-        const num = saved[name]
-        return typeof num == "number" ? num : Math.max()
+    const {all,current} = await gitWithoutStdout.branch(["--sort=-committerdate"]);
+    const choices = all
+        .filter(e => e != current)
+        .map(e => ({title:e,value:e}))
+    if(!choices.length){
+        console.log("No branch to checkout")
+        return;
     }
-    const sortee = all.filter(e => e != current)
-    sortee.sort((a,b)=> getPriority(a) - getPriority(b))
-    const choices = sortee.map(e => ({title:e,value:e}))
     const { branch } = await prompts([
         {
             type: "select",
@@ -92,7 +26,6 @@ const checkout = async () => {
     ]);
     if(branch){
         git.checkout([branch]);
-        repo.add(branch);
     }
 }
 
@@ -104,22 +37,30 @@ const newBranch = async () => {
             name: "name"
         }
     ]);
-    git.checkoutLocalBranch(name);
-    repo.add(name);
+    if(name){
+        git.checkoutLocalBranch(name);
+    }
 }
 
 const deleteBranch = async () => {
-    const {all} = await gitWithoutStdout.branch();
+    const {all,current} = await gitWithoutStdout.branchLocal();
+    const choices = all
+        .filter(e => e != current)
+        .map(e => ({title:e,value:e}))
+    if(!choices.length){
+        console.log("No branch to delete")
+        return;
+    }
     const { branches } = await prompts([
         {
             type: "multiselect",
             message: "Pick branches",
             name: "branches",
-            choices:all.map(e => ({title:e,value:e}))
+            choices
         }
     ]);
     if(branches.length){
-        git.deleteLocalBranches(branches)
+        git.deleteLocalBranches(branches,true)
     }
 }
 
@@ -145,18 +86,15 @@ const fn = async () => {
     })
     switch (cmd) {
         case "checkout":
-            checkout();
-            break;
+            return checkout();
         case "new":
-            newBranch()
-            break;
+            return newBranch()
         case "delete":
-            deleteBranch()
-            break;
+            return deleteBranch();
         default:
             throw new Error(cmd)
     }
 };
-fn()
+fn().catch(() => {})
 
 
