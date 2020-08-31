ad51891bbc1b0d3f42867a762ea44ceea7cab671
haga-itsuki <haga-itsuki@dmm.com>
2020-08-31 15:48:29 +0900

    iran

--- src/scripts/commit.ts
+++
@@ -1,7 +0,0 @@
-import cp from "child_process";
-
-export const run = () => {
-    const args = process.argv.slice(2)
-    // simple-gitはviの画面を出せない
-    cp.spawn("git", args.length ? ["commit", "-m", ...args] : ["commit"], { stdio: 'inherit' })
-}
